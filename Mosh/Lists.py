966bb57e323381f5b19afa16b972d8bcaf2e82f7
Tetroner9 <omkadam2125@gmail.com>
2023-03-07 01:04:56 +0530

    m

---
+++ Mosh/Lists.py
@@ -0,0 +1,7 @@
+numbers = [1, 45, 33, 77, 4]
+max = numbers[0]
+for num in numbers:
+    if num > max:
+        max = num
+
+print(max)
---
+++ Mosh/NestedLoops.py
@@ -0,0 +1,7 @@
+for x in range(4):
+    for y in range(3):
+        print(f"{x},{y}")
+
+numbers = [1, 2, 3, 4, 5, 6]
+for x_count in numbers:
+    print('*'*x_count)
