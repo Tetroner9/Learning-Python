78952ad44c5869daa607cd6b899815c19a28560f
Tetroner9 <omkadam2125@gmail.com>
2023-04-24 02:31:15 +0530

    ok

--- Python/Exp-04/3.py
+++ Python/Exp-04/3.py
@@ -1,7 +1,7 @@
 import os
 import string
 
-path = 'E:/Programming/Python/Exp-4/3'
+path = '/Exp-04/3'
 os.chdir(path)
 for letter in string.ascii_uppercase:
     with open(letter + ".txt", "w") as f:
--- Python/Exp-7/quiz_data.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-parameters = {
-    "amount": 5,
-    "type": "multiple"
-}
-
-response = requests.get(url="https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple", params=parameters)
-question_data = response.json()["results"]
